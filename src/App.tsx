396748b0804592265d4c5ed8d9ac4615394da937
cabetorelofcrisfel <cabetorelofcrisfel@gmail.com>
2025-09-16 22:21:38 +0100

    Resolvendo conflitos

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,4 @@
-﻿function RequireAdminHospital({ children }: { children?: React.ReactNode }) {
-  const isAuth = localStorage.getItem("moyo-auth") === "true";
-  const userPerfil = localStorage.getItem("moyo-perfil");
-  const location = useLocation();
-  if (!isAuth || userPerfil !== "adminhospital") {
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  }
-  return <Outlet />;
-}
-import React from "react";
+﻿import React from "react";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import Dashboard from "./features/dashboard/Dashboard";
@@ -23,12 +14,9 @@ import Signup from "./features/auth/Signup";
 import { Routes, Route, useLocation, Navigate, Outlet } from "react-router-dom";
 import PacienteDashboard from "./features/paciente/PacienteDashboard";
 import AdminDashboard from "./features/DasboardAdmin/AdminDashboard";
-<<<<<<< HEAD
 import AdminHospitalProfile from "./features/adminDashboardhospital/DashboardAdminHospital";
 import ProfissionaisAdmin from "./features/adminDashboardhospital/ProfissionaisAdmin";
 
-=======
->>>>>>> cf88536afe7e14b64ba0d90336c657939ec9024f
 
 function RequireAuth({ perfil }: { children?: React.ReactNode; perfil: string }) {
   const isAuth = localStorage.getItem("moyo-auth") === "true";
@@ -97,12 +85,10 @@ function App() {
         </Route>
       </Route>
       <Route path="/admin" element={<AdminDashboard />} />
-<<<<<<< HEAD
-      <Route path="/adminhospitaldashboard" element={<AdminHospitalProfile />} />
-      <Route path="/ProfissionaisAdmin" element={<ProfissionaisAdmin />} />
+      <Route path="/adminhospital" element={<AdminHospitalProfile />} />
+      <Route path="/adminhospital/dashboard" element={<AdminHospitalProfile />} />
+      <Route path="/adminhospital/profissionais" element={<ProfissionaisAdmin />} />
       <Route path="/admin/profissionais" element={<ProfissionaisAdmin />} />
-=======
->>>>>>> cf88536afe7e14b64ba0d90336c657939ec9024f
     </Routes>
   );
 }
